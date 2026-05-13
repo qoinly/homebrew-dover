1a3b8cdf1c218f23a712286836ee403df9193235
Ramadhan Sutejo <ramadhansutejo94@gmail.com>
2026-05-13 22:53:30 +0700

    release v0.18.10

--- Casks/dover.rb
+++ Casks/dover.rb
@@ -1,13 +1,13 @@
 # Casks/dover.rb
 cask "dover" do
-  version "0.18.4"
+  version "0.18.10"
 
   if Hardware::CPU.arm?
     url "https://github.com/qoinly/homebrew-dover/releases/download/v#{version}/Dover-#{version}-darwin-arm64.zip"
-    sha256 "e308ad16150ba4900e3578e3bc6960d63cfd176a77ee6d069e9d7e2c2a6c00f5"
+    sha256 "7332251f20e19feaa9798cd501d91771a8c3a2bf49c24c6b3480e44533b76f40"
   else
     url "https://github.com/qoinly/homebrew-dover/releases/download/v#{version}/Dover-#{version}-darwin-amd64.zip"
-    sha256 "cd34222ffb7d640d959dd8d7898007e72aefd1572a996f3cd80cc087d156109b"
+    sha256 "8487040b9879f23cd4dbede5eec99176a22a196e0c3c8eee3c0570b4db2da6bb"
   end
 
   name "Dover"
